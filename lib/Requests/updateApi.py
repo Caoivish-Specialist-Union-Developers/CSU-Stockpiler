216f5f4a9aa4defc6dd7f2aa5fe64c587c38c745
ianfights <ianfights@gmail.com>
2023-03-04 21:33:31 -0500

    Fixed API Update

--- lib/Requests/updateApi.py
+++ lib/Requests/updateApi.py
@@ -1,29 +1,23 @@
 import requests
-
 def updateApi(menu, items, ThisStockpileName):
     if menu.updateAPI.get() == 1:
-        requestObj = {
-            "key": menu.APIKey.get(),
-        }
         
-
         data = []
         for x in items.sortedcontents:
             data.append({"id":x[0], "item":x[1], "amount": x[2]})
-            requestObj["data"] = data
 
 
-            finData = {"name": ThisStockpileName, "contents": data }
+        finData = {"name": ThisStockpileName, "contents": data }
 
-            print("Bot Data", finData)
+        print("Bot Data", finData)
 
-            print(requestObj)
+        print(menu.APIHost.get()+'?key='+menu.APIKey.get(), finData)
 
-            try:
-                r = requests.post(menu.BotHost.get()+'?key='+menu.APIKey, finData)
-                response = r.json()
-                print(response)
-                print("sending")
-            except Exception as e:
-                print("There was an error connecting to the API")
-                print("Exception: ", e)
+        try:
+            r = requests.post(menu.APIHost.get()+'?key='+menu.APIKey.get(), json=finData)
+            response = r
+            print(response)
+            print("sending")
+        except Exception as e:
+            print("There was an error connecting to the API")
+            print("Exception: ", e)
